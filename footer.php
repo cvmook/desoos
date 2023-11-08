350b3925480cae5062bfec2562020beec782c21b
JaimyZ <224990@student.scalda.nl>
2023-11-08 11:08:19 +0100

    .

---
+++ footer.php
@@ -0,0 +1,32 @@
+<?php
+include("index.php");
+?>
+<div class="footer">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="links-footer">
+            <h2 class="footer-title">Links</h2>
+            <h3><a class="footer-pages" href="#">Home</a></h3>
+            <h3><a class="footer-pages" href="#">Activiteiten</a></h3>
+            <h3><a class="footer-pages" href="#">Over ons</a></h3>
+            <h3><a class="footer-pages" href="#">Foto's</a></h3>
+
+        </div>
+    </div>
+    <div class="col-sm-4"> 
+        <div class="contact-footer">
+            <h2 class="footer-title"> Contact</h2>
+            <p>Emailadres: Example@gmail.com</p>
+            <br>
+            <p>Adres: Nieuwstraat<br>4493 BE Kamperland</p>
+            <button class="contact-button"><p class="button-text">Neem contact op</p></button>
+        </div>
+</div>
+   
+
+    <div class="col-sm-4">
+        <div class="footer-joehoe">
+        <h2 class="footer-title"> joehoeee</h2>
+    </div>
+    </div>
