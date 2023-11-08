d70771fc14bdaa15fb800de5f3f50efc23b9ee6a
yannick-elias <yannickelias@outlook.com>
2023-11-08 11:04:10 +0100

    .homepage code

---
+++ public/assets/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="De soos is u kunt hier u kind inschrijven voor leuke activiteiten in kamperland.">
+    <meta name="keywords" content="keywords, for, SEO">
+    <meta name="author" content="Yannick Elias, Casper van Mook, Jaimy Zegers">
+
+    <!-- Favicon -->
+    <link rel="icon" href="" type="image">
+    
+    <!-- CSS Stylesheets -->
+    <link rel="stylesheet" href="css/reset.css">
+    
+    <!-- CSS and JavaScript libraries (if needed) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+
+    <!-- ******** Navigation ******** -->
+    <div class="m-4" id="">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <a href="index.php" class="navbar-brand">
+                    <img id="logo" src="img\logo.png" alt="Parfumerie van biemen logo">
+                </a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto">
+                        <a href="index.php" class="nav-item nav-link">Home</a>
+                        <a href="geuren.php" class="nav-item nav-link">Geuren</a>
+                        <a href="cosmetica.php" class="nav-item nav-link">Cosmetica</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
